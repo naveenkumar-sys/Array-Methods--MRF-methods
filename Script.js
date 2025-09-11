a60d0da0fdc8db1130f86d8c9a17551832a8b82e
naveenkumar-sys <naveenrnk69@gmail.com>
2025-09-11 13:13:37 +0530

    Updated

--- Script.js
+++ Script.js
@@ -1,51 +1,112 @@
-                // Array methods
-/* 1.Map()-Transforming Data
-           => The map() method creates a new array by applying a function to each element in the original array.
+/*
+ 1. [mangoe, orange, apple] add the word S to end of each string.
+ 2. [10,11,12,89,35,156,199,25,32,87] Find the numbers which are greater than 80 from given array.
+ 3. [10,12,15,16,8,11] Find the largest/ greates number in an array. 
 
-            Syntax: array.map(function(element, index, array) {
-              return new value
-            });
-
-
-           => Use case: Modify or transform array elements (e.g., changing units, formatting data).
-
-
-Example: map method using Arrow function
-*/ 
-            let array=[2,4,6,8];
-
-            const new_array=array.map(num=>num*num);
-            console.log(new_array);
-
-
- /* 2. filter(): Selecting Data
-            =>The filter() method creates a new array with elements that meet a specific condition.
-            Syntax:
-                        array.filter(function(element, index, array) {
-                                return true to keep the element
-                                return false to remove it
-                            });
- 
-        =>  Use case: Extract relevant data (e.g., removing inactive users, selecting premium subscribers).
- Example: 
  */
 
-            let arr2=[2,5,4,6,8,10,15];
-            let even_array=arr2.filter(num=>num%2==0);
-            console.log(even_array);
 
-  /*  reduce(): Summarizing Data
-            =>The reduce() method reduces an array to a single value (e.g., sum, average, maximum).
+//1. [mangoe, orange, apple] add the word S to end of each string.
+//Normal method without using map method
+const arr=["mango","orange","apple"];
+let new_array=[];
+function add(arr1){
+  for(let i=0;i<arr.length;i++)
+  {
+     new_array.push(arr[i]+"s");
+  }
+  return new_array;
+}
+let result=add()
+console.log(result);
 
-            Syntax:
-                        array.reduce(function(accumulator, currentValue, index, array) {
-                            return updated accumulator
-                        }, initialValue);
 
-            Use case: Aggregate data (e.g., calculating totals, finding averages).
- Example: 
- */
-            let numbers = [1, 2, 3, 4, 5];
-            let sum = numbers.reduce((acc, num) => acc + num, 0);
-            console.log(sum)
+//1. Normal function with map Method
+const ar=["mango","orange","apple"];
+let result1=ar.map(function normal(i)
+{
+    return i+"s";
+})
+console.log(result1);
+
+//2.Anonymous function using map method
+
+const array=["mango","orange","apple"];
+let result2=array.map(function(ele)
+{
+    return ele+"s";
+})
+console.log(result2);
+
+//3.Arrow method using map method
+const arr1=["mango","orange","apple"];
+
+const res_final=arr1.map((element)=>{
+  return element+"s";
+})
+console.log(res_final);
+
+
+// 2. [10,11,12,89,35,156,199,25,32,87] Find the numbers which are greater than 80 from given array.
+
+//1.normal function using filter method
+const arr_list=[10,11,12,89,35,156,199,25,32,87];
+
+let result4=arr_list.filter(function larger(numbers)
+{
+    return numbers>=80;
+})
+console.log(result4);
+
+//2.Anonymous Function || Expression Function using filter method
+const arr2_list=[10,11,12,89,35,156,199,25,32,87];
+
+let result5=arr2_list.filter(function(number)
+{
+    return number>=80;
+})
+console.log(result5);
+
+
+// 3.Arrow method using filter method
+ let arr2=[10,11,12,89,35,156,199,25,32,87];
+
+ let res=arr2.filter((elements)=>{
+    return elements>=80;
+ })
+ console.log(res);
+
+ //qn3
+
+ //Normal function without using reduce method
+const arr3=[10,12,15,16,8,11];
+let largest=0;
+ function greatest(arr3){
+   for(let i=0;i<arr3.length;i++)
+   {
+     if(arr3[i]>largest)
+     {
+       largest=arr3[i];
+     }
+   }
+   console.log( largest);
+}
+greatest(arr);
+
+
+// 3. [10,12,15,16,8,11] Find the largest/ greates number in an array. 
+
+//Arrow function using reduce method
+ let arr4=[10,12,15,16,8,11];
+ let final=arr4.reduce((max ,value)=>{
+    if(value>max){
+        max=value;
+    }
+    return max;
+ },arr4[0])
+ console.log(final);
+
+
+
+
 
